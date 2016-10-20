12610d60d01e3e57538bdca350614f260080618b
Shinya Ohyanagi <heavenshell.jp@gmail.com>
2016-10-21 00:10:03 +0900

    Add storybook and knobs addon
    `storybook-addon-knobs` does not publish definiton file.
    
    So add `d.ts` file local.
    
    To enable local `d.ts`, add `paths` to `tsconfig.json`.

---
+++ types/storybook-addon-knobs/index.d.ts
@@ -0,0 +1,30 @@
+export class KnobManager {
+  constructor();
+  knob(name: any, options: any): any;
+  wrapStory(channel: any, storyFn: any, context: any): any;
+}
+
+export class KnobStore {
+  constructor();
+  has(key: string): boolean;
+  set(key: string, value: any): void;
+  get(key: string): any;
+  getAll(): any;
+  reset(): void;
+  markAllUnused(): void;
+  subscribe(cb: any): void;
+  unsubscribe(cb: any): void;
+}
+
+export function text(name: string, value: any): any;
+export function boolean(name: string, value: any): any;
+export function number(name: string, value: any): any;
+export function object(name: string, value: any): any;
+export function select(name: string, options: any): any;
+export function date(name: string, value: Date): any;
+
+interface Context {
+  kind: string
+  story: string
+}
+export function withKnobs(storyFn: any, context: Context): any;
